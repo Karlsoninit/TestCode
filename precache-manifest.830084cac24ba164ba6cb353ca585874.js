1e6f3a8084cfc8d5ff98402206923c0b0e04e912
MacBook <macbook@MacBook-Pro-MacBook.local>
2019-07-11 23:37:57 +0300

    Updates

---
+++ precache-manifest.830084cac24ba164ba6cb353ca585874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557f46e48503920b4e9f0d0896f3a0e3",
+    "url": "/TestCode/./index.html"
+  },
+  {
+    "revision": "0d1ebb954f08fd3220f2",
+    "url": "/TestCode/./static/css/main.96e46926.chunk.css"
+  },
+  {
+    "revision": "3805b13e5c8d70c36254",
+    "url": "/TestCode/./static/js/2.d3c8313b.chunk.js"
+  },
+  {
+    "revision": "0d1ebb954f08fd3220f2",
+    "url": "/TestCode/./static/js/main.97404b4c.chunk.js"
+  },
+  {
+    "revision": "b02c4f6ecca6c6787207",
+    "url": "/TestCode/./static/js/runtime~main.2932cccc.js"
+  }
+]);
